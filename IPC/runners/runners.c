b47377d9a2e21660b474a8ff997c1e529444beac
Zakhar <z.inform@mail.ru>
2021-10-27 12:26:29 +0300

    Runners done

--- IPC/runners/runners.c
+++ IPC/runners/runners.c
@@ -7,7 +7,12 @@
 #include <stdlib.h>
 
 int judge(int run_count, int q_id);
-int runner(int runner_id, int q_id);
+int runner(int runner_id, int q_id, long judge_id);
+
+struct dummy_msg_struct {
+    long mtype;
+    char mtext[0];
+};
 
 int main(int argc, char* argv[]){
 
@@ -18,31 +23,31 @@ int main(int argc, char* argv[]){
 
     int run_count = atoi(argv[1]);
     
-    //set up queue
-    int q_id = msgget(IPC_PRIVATE, IPC_CREAT | 0660); //rw-rw----
+    int q_id = msgget(IPC_PRIVATE, IPC_CREAT | 0660); /*rw-rw----*/
     
-    for (int i = 0; i < run_count; i++){
+    for (int i = 1; i <= run_count; i++){
         int pid = fork();
         if (!pid) {
-            //runner();
+            runner(i, q_id, (long) run_count + 1);
             return 0;
         }
     }
 
-    //judge();
-    //close queue
+    judge(run_count, q_id);
+    msgctl(q_id, IPC_RMID, NULL);
     
     return 0;
 }
 
 int judge(int run_count, int q_id){
+    struct dummy_msg_struct dummy_msg;
     printf("Judge on the field\n");
     for (long i = 1; i <= run_count; i++) 
-        if (msgrcv(q_id, NULL, 0, i, MSG_NOERROR) == -1){ //wait for all runners
+        if (msgrcv(q_id, &dummy_msg, 0, run_count + 1, MSG_NOERROR) == -1){ /*wait for all runners*/
             perror("judge rcv error");
             return -1;
         }
-    printf("All runners present\nStarting the run");
+    printf("All runners present\nStarting the run\n");
 
     long mtype = 1;
     if (msgsnd(q_id, &mtype, 0, 0) == -1){
@@ -51,25 +56,50 @@ int judge(int run_count, int q_id){
     }
     printf("First runner to start\n");
 
-    if (msgrcv(q_id, NULL, 0, run_count, MSG_NOERROR) == -1){
+    if (msgrcv(q_id, &dummy_msg, 0, run_count + 1, MSG_NOERROR) == -1){
         perror("judge rcv error");
         return -1;
     }
 
     printf("Last runner finished\n");
+    printf("Run ended\n");
     for (long i = 1; i <= run_count; i++)
         if (msgsnd(q_id, &i, 0, 0) == -1){
             perror("judge snd error");
             return -1;
         }
-    printf("Run ended");
     return 0;
 }
 
-int runner(int runner_id, int q_id){
+int runner(int runner_id, int q_id, long judge_id){
+    struct dummy_msg_struct dummy_msg;
+    printf("Runner %d arrived\n", runner_id);
+    long mtype = judge_id;
+    if (msgsnd(q_id, &mtype, 0, 0) == -1) {
+        perror("runner send error");
+        return -1;
+    }
 
+    mtype = (long) runner_id;
+    if (msgrcv(q_id, &dummy_msg, 0, mtype, MSG_NOERROR) == -1) {
+        perror("runner rcv error");
+        return -1;
+    }
 
+    mtype++;
+    if (msgsnd(q_id, &mtype, 0, 0) == -1) {
+        perror("runner snd error");
+        return -1;
+    }
 
+    printf("Runner %d passed\n", runner_id);
+
+    if (msgrcv(q_id, &dummy_msg, 0, (long) runner_id, MSG_NOERROR) == -1) {
+        perror("runner rcv error");
+        return -1;
+    }
+
+    printf("Runner %d left\n", runner_id);
 
     return 0;
 }
