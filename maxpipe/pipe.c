478a6b1f204ce0314a4c16b0dff0dd6ed2119ab3
Zakhar <z.inform@mail.ru>
2022-01-06 03:59:53 +0300

    pipe dump to file added

--- maxpipe/pipe.c
+++ maxpipe/pipe.c
@@ -6,6 +6,7 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 #include <errno.h>
 #include <stdio.h>
@@ -30,7 +31,7 @@ int main(){
         return -1;
     }
 
-    //fcntl(pipes[0], F_SETPIPE_SZ, 32768);
+    fcntl(pipes[0], F_SETPIPE_SZ, 32768);
     printf("System pipe size %d\n", fcntl(pipes[0], F_GETPIPE_SZ));
 
     int pid = fork();
@@ -45,18 +46,38 @@ int main(){
     unsigned char byte;
     int size = 0;
 
+    struct timespec timeout = {10, 0};
+    int fd = open("pipedump", O_WRONLY);
+    if (fd == -1) {
+        perror("file open error");
+        return -1;
+    }
+
     while(1){
-        semop(semid, byte_rcv, 1);
+        if ((semtimedop(semid, byte_rcv, 1, &timeout) == -1) && (errno == EAGAIN)) {
+            printf("%d\n", size);
+            printf("Semaphore operations broke\n");
+            if (splice(pipes[0], NULL, fd, NULL, 32768, SPLICE_F_NONBLOCK) == -1) {
+                perror("splice error");
+                return -1;
+            }
+            printf("Pipe dumped\n");
+            printf("attempting to revive by reading and rewriting a byte\n");
+            read(pipes[0], &byte, 1);
+            write(pipes[1], &byte, 1);
+        }
         size++;
         usleep(500);
         errno = 0;
         if ((semop(semid, check_blocked, 1) == -1) && (errno != EAGAIN)) {
+            printf("%d\n", size);
             perror("wah problems");
             return -1;
         }
         if (errno == EAGAIN) {
+            printf("%d\n", size);
             printf("possible max reached\n");
-            usleep(1000);
+            usleep(1000000);
             errno = 0;
             semop(semid, check_blocked, 1);
             if (errno == EAGAIN) {
@@ -68,6 +89,11 @@ int main(){
                     printf("Не ну тут уже планировщик вообще меня обижает, я так работать не буду\n");
                     break;
                 }
+                if (splice(pipes[0], NULL, fd, NULL, 32768, SPLICE_F_NONBLOCK) == -1) {
+                    perror("splice error");
+                    return -1;
+                }
+                printf("Pipe dumped\n");
                 printf("pipe size %d bytes\n", size - 1);
                 break;
             }
@@ -75,7 +101,6 @@ int main(){
         }
         read(pipes[0], &byte, 1);
         semop(semid, cycle_done, 1);
-        printf("%d\n", size);
     }
 
     kill(pid, SIGTERM);
@@ -84,16 +109,16 @@ int main(){
 }
 
 int watchdog(int semid, int pipes[2]){
-    unsigned char byte = 0xEE;
+    unsigned char byte = 0;
     struct sembuf byte_sent[1] = {{0, 1, 0}};
     struct sembuf cycle_wait[1] = {{1, -1, 0}};
-    
     while(1){
         write(pipes[1], &byte, 1);
         semop(semid, byte_sent, 1);
         write(pipes[1], &byte, 1);
         semop(semid, byte_sent, 1);
         semop(semid, cycle_wait, 1);
+        byte++;
     }
 }
 
